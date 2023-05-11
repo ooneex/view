6f35d78fdae97794809de8db90b0ac2a08877c03
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-11 21:26:46 +0300

    feat: add view directories

--- config/app/app.config.ts
+++ config/app/app.config.ts
@@ -1,10 +1,14 @@
 const config = {
   directories: {
     "root": new URL("../..", import.meta.url).pathname.replace(/\/\/$/, "/"),
+    "components": "components",
     "config": "config",
     "controllers": "controllers",
+    "islands": "islands",
     "middlewares": "middlewares",
+    "public": "public",
     "var": "var",
+    "views": "views",
   },
 };
 
--- config/app/types.ts
+++ config/app/types.ts
@@ -1,9 +1,13 @@
 export type AppDirectoryType = {
   root: string;
+  components: string;
   config: string;
   controllers: string;
+  islands: string;
   middlewares: string;
+  public: string;
   var: string;
+  views: string;
 };
 
 export type AppConfigType = {
