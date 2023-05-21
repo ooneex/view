c1b6b38e19ed990c56cd361cafe0640c9c25189c
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-21 07:02:52 +0300

    feat: pass some env values

--- index.ts
+++ index.ts
@@ -1,22 +1,15 @@
-import { EnvHelper } from "@ooneex/env";
 import { print } from "@ooneex/exception";
 import { Server } from "@ooneex/http";
-import { watchIslands } from "@ooneex/island";
 import { Kernel } from "@ooneex/kernel";
 
-Deno.env.set("ROOT_DIR", new URL(".", import.meta.url).pathname);
+Deno.env.set("OONEEX_APP_TYPE", "view");
+Deno.env.set("OONEEX_APP_ROOT_DIR", new URL(".", import.meta.url).pathname);
 
 try {
   await Kernel.boot();
 } catch (e) {
   print(e);
   Deno.exit(1);
-}
-
-const envHelper = new EnvHelper();
-
-if (envHelper.isLocal()) {
-  await watchIslands();
 }
 
 try {
