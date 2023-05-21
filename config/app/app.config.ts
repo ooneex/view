5202e8a5590c5151b190b00df05ee3010e9d9212
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-21 06:58:54 +0300

    feat(config): type default config

--- config/app/app.config.ts
+++ config/app/app.config.ts
@@ -1,6 +1,8 @@
-const config = {
+import { AppConfigType } from "./types.ts";
+
+const config: AppConfigType = {
   directories: {
-    "root": new URL("../..", import.meta.url).pathname.replace(/\/\/$/, "/"),
+    "root": new URL("../..", import.meta.url).pathname.replace(/\/+$/, ""),
     "components": "components",
     "config": "config",
     "controllers": "controllers",
