14535f08c5ce044a52df1ccfbc83cd012b81eb9c
fkdiomande <fkdiomande.dev@gmail.com>
2023-05-22 21:45:33 +0300

    fix(controller): use Request interface

--- controllers/HomepageController.ts
+++ controllers/HomepageController.ts
@@ -1,11 +1,12 @@
 import { Route } from "@ooneex/decorator";
-import { Request, Response as HttpResponse } from "@ooneex/http";
+import type { IRequest } from "@ooneex/http";
+import { Response as HttpResponse } from "@ooneex/http";
 import { HomepageView, IHomepageViewProps } from "@views";
 
 export class HomepageController {
   @Route("homepage", "/")
   public async index(
-    _request: Request,
+    _request: IRequest,
     response: HttpResponse,
   ): Promise<Response> {
     return await response.render<IHomepageViewProps>(HomepageView, {
